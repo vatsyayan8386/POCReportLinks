21cd6b1a9760ef79ace24803c0780afe009e3b6d
Ashish <41682277+AshishKr2303@users.noreply.github.com>
2020-09-27 16:48:30 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,74 @@
+from flask import Flask, render_template, url_for, flash, redirect, session, Blueprint
+from sklearn.externals import joblib
+from flask import request
+import numpy as np
+import os
+from chatbot import CB
+
+from flask_session import Session
+app = Flask(__name__, template_folder='template',static_folder='static')
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+
+#@app.route('/Files/<path:filename>')
+#def download(filename):
+ #   return send_from_directory(directory='Files', filename=filename)
+
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template("home.html")
+
+
+@app.route("/AbbvieDM_password")
+def AbbvieDM_password():
+    return render_template("AbbvieDM_password.html")
+
+
+
+@app.route("/AbbvieDM")
+def AbbvieDM():
+    return render_template("AbbvieDM.html")
+
+@app.route("/AcuityBrandsWellness_password")
+def AcuityBrandsWellness_password():
+    return render_template("AcuityBrandsWellness_password.html")
+
+@app.route("/AcuityBrandsWellness")
+def AcuityBrandsWellness():
+    # if form.validate_on_submit():
+    return render_template("AcuityBrandsWellness.html")
+
+@app.route("/ApexToolHCCR_password")
+def ApexToolHCCR_password():
+    # if form.validate_on_submit():
+    return render_template("ApexToolHCCR_password.html")
+
+@app.route("/ApexToolHCCR")
+def ApexToolHCCR():
+    # if form.validate_on_submit():
+    return render_template("ApexToolHCCR.html")
+
+@app.route("/MastercardWellness_password")
+def MastercardWellness_password():
+    return render_template("MastercardWellness_password.html")
+
+@app.route("/MastercardWellness")
+def MastercardWellness():
+    return render_template("MastercardWellness.html")
+
+@app.route("/chatbot")
+def chatbot():
+    return render_template("chatbot.html")
+
+
+
+@app.route("/get")
+def get_bot_response():
+    userText = request.args.get('msg')
+    return str(CB.get_response(userText))
+
+
+if __name__ == "__main__":
+    app.run(debug=True,port=5001)
---
+++ chatbot.py
@@ -0,0 +1,45 @@
+from chatterbot import ChatBot
+from chatterbot.trainers import ListTrainer
+from chatterbot.trainers import ChatterBotCorpusTrainer
+import spacy
+# Creating ChatBot Instance
+CB = ChatBot('ChatBot')
+ # Training with Personal Ques & Ans
+conversation = [
+    "Hi",
+    "Hi there, Do you want to run a Standard Report, Press Yes!",
+    "Yes",
+    "Thank You,Please mention the report name in Capital Letters",
+    "STANDARDQUITFORLIFE",
+    "Please mention the Policy Number",
+    "0012345",
+    "Please mention the Start Date in mmddyyyy format",
+    "01012020",
+    "Please mention the End Date in mmddyyyy format",
+    "01312020",
+    "Thank you for your Inputs, Your report will run shortly, Please check in the output folder",
+    "Thank You",
+    "Thank you , Have a Great Day ahead!"
+]
+trainer = ListTrainer(CB)
+trainer.train(conversation)
+# # Training with English Corpus Data
+trainer_corpus = ChatterBotCorpusTrainer(CB)
+#trainer_corpus.train('chatterbot.corpus.english')
+
+
+'''
+from flask import Flask, render_template, request
+
+app = Flask(__name__,template_folder='template')
+
+@app.route("/")
+def home():
+    return render_template("chatbot.html")
+
+@app.route("/get")
+def get_bot_response():
+    userText = request.args.get('msg')
+    return str(CB.get_response(userText))
+app.run(debug = True,port=5003)
+'''
